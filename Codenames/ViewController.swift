0f220a3f7eee675266bc2c7a6b57e33609b172d4
mdeshina <deshinamash>
2020-09-04 12:30:21 +0300

    Initial Commit

---
+++ Codenames/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Codenames
+//
+//  Created by Машенька on 04.09.2020.
+//  Copyright © 2020 Машенька. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
